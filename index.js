66ebb5fe0a2d02996d610d95a573173bb0ed3568
Sebastian Perez <sebastianteres@me.com>
2014-02-22 03:47:50 -0600

    Fixed paths

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.engine('.html', require('ejs').__express);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/app/views');
 app.set('view engine', 'html');
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app/public'));
 
 function staticView(view) {
 	return function(req, res){
